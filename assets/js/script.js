57c144e3a8263297c62cde6ce03b95b3bd2c2764
Haguermeister <cadethague70@gmail.com>
2021-12-15 18:21:26 -0500

    finished moving tasks to columns

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,46 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var taskIdCounter = 0;
+var pageContentEl = document.querySelector("#page-content");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 function taskFormHandler() {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']");
     var taskTypeInput = document.querySelector("select[name='task-type']");
-    
-    if(!taskNameInput.value || !taskTypeInput.value){
+
+    if (!taskNameInput.value || !taskTypeInput.value) {
         alert("You need to fill out the task form!");
         return false;
     }
 
-    //package data into object
-    var taskDataObj = {
-        name: taskNameInput.value,
-        type: taskTypeInput.value
-    };
+    var isEdit = formEl.hasAttribute("data-task-id");
+
+    if (isEdit) {
+        var taskId = formEl.getAttribute("data-task-id");
+        completeEditTask(taskNameInput.value, taskTypeInput.value, taskId);
+    }
+    else {
+        var taskDataObj = {
+            name: taskNameInput.value,
+            type: taskTypeInput.value
+        };
+
+        createTaskEl(taskDataObj);
+    }
     formEl.reset();
 
-    createTaskEl(taskDataObj);
+
 };
 
-function createTaskEl(taskDataObj){
+function createTaskEl(taskDataObj) {
     //Create list Item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
+
+    //add data attribute of task-counter
+    listItemEl.setAttribute("data-task-id", taskIdCounter);
 
     //Create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
@@ -35,8 +51,122 @@ function createTaskEl(taskDataObj){
 
     listItemEl.appendChild(taskInfoEl);
 
+    var taskActionsEl = createTaskActions(taskIdCounter);
+    taskIdCounter++;
+
+    listItemEl.appendChild(taskActionsEl);
     // add entire item to list
     tasksToDoEl.appendChild(listItemEl);
 }
 
+function createTaskActions(taskId) {
+    var actionContainerEl = document.createElement("div");
+    actionContainerEl.className = "task-actions";
+
+    var editButtonEl = document.createElement("button");
+    editButtonEl.textContent = "Edit";
+    editButtonEl.className = "btn edit-btn";
+    editButtonEl.setAttribute("data-task-id", taskId);
+
+    actionContainerEl.appendChild(editButtonEl);
+
+    var deleteButtonEl = document.createElement("button");
+    deleteButtonEl.textContent = "Delete";
+    deleteButtonEl.className = "btn delete-btn";
+    deleteButtonEl.setAttribute("data-task-id", taskId);
+
+    actionContainerEl.appendChild(deleteButtonEl);
+
+    var statusSelectEl = document.createElement("select");
+    statusSelectEl.className = "select-status";
+    statusSelectEl.setAttribute("name", "status-change");
+    statusSelectEl.setAttribute("data-task-id", taskId);
+
+    actionContainerEl.appendChild(statusSelectEl);
+
+    var statusChoices = ["To Do", "In Progress", "Completed"];
+
+    for (var i = 0; i < statusChoices.length; i++) {
+        var statusOptionEl = document.createElement("option");
+        statusOptionEl.textContent = statusChoices[i];
+        statusOptionEl.setAttribute("value", statusChoices[i]);
+
+        statusSelectEl.appendChild(statusOptionEl);
+    }
+
+    return actionContainerEl;
+}
+
+function taskButtonHandler(event) {
+
+    var targetEl = event.target;
+
+    if (targetEl.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+    if (targetEl.matches(".edit-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        editTask(taskId);
+    }
+
+}
+
+function deleteTask(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+}
+
+function editTask(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+
+    document.querySelector("input[name='task-name']").value = taskName;
+    document.querySelector("select[name='task-type']").value = taskType;
+    document.querySelector("#save-task").textContent = "Save Task";
+
+    formEl.setAttribute("data-task-id", taskId);
+
+
+}
+function completeEditTask(taskName, taskType, taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    taskSelected.querySelector("h3.task-name").textContent = taskName;
+    taskSelected.querySelector("span.task-type").textContent = taskType;
+
+    alert("Task Updated!");
+
+    formEl.removeAttribute("data-task-id");
+    document.querySelector("#save-task").textContent = "Add Task";
+}
+function taskStatusChangeHandler(event) {
+    var taskId = event.target.getAttribute("data-task-id");
+    console.log(event.target.value);
+    var statusValue = event.target.value.toLowerCase();
+    console.log(statusValue);
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    if (statusValue === "to do") {
+        tasksToDoEl.appendChild(taskSelected);
+    }
+    else if (statusValue === "in progress") {
+        tasksInProgressEl.appendChild(taskSelected);
+    }
+    else if (statusValue === "completed") {
+        tasksCompletedEl.appendChild(taskSelected);
+    }
+}
+
 formEl.addEventListener("submit", taskFormHandler);
+pageContentEl.addEventListener("click", taskButtonHandler)
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+
+var taskId = 1;
+
+var divEl = document.createElement("div");
+divEl.className = "task-item";
+divEl.setAttribute("data-id", taskId);
+console.log(divEl[data-id]);
